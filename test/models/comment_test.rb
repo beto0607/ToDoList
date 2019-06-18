f4f9c32e71cecbc046d2461d2c5de1367851eb4c
Roberto Albanesi <beto0607@gmail.comn>
2019-06-18 11:34:07 +0200

    Added tests to comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "Should create comment" do
+        assert build(:comment).valid?
+    end
+    test "Shouln't create comment without user" do
+        assert_not build(:comment, user:nil).valid?
+    end
+    test "Shouln't create comment without list" do
+        assert_not build(:comment, list:nil).valid?
+    end
+    test "Shouln't create comment without text" do
+        assert_not build(:comment, description:nil).valid?
+    end
 end
