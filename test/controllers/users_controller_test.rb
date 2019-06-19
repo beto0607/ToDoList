c1c17ea1a72a6f5b3a948c82b3d6339649641239
Roberto Albanesi <beto0607@gmail.comn>
2019-06-19 13:22:46 +0200

    Spelling corrections and naming consistency

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,136 +8,146 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
     #Index
-    test "Index - ok" do
+    test "Index - Ok" do
         createUserAndLogin
         get users_url, headers: @auth_header
         assert_response :ok
     end
-    test "Index - It must be authenticated" do
+    test "Index - User should be logged in" do
         get users_url
         assert_response :unauthorized
     end
     #End index
     #Show
-    test "Show - ok" do
+    test "Show - Ok" do
         createUserAndLogin
         get user_url(@user), headers: @auth_header
         assert_response :ok
     end
-    test "Show - It must be authenticated" do
+    test "Show - User should be logged in" do
         @user = create(:user)
         get user_url(@user)
         assert_response :unauthorized
     end
-    test "Show - not found" do
+    test "Show - Not found" do
         createUserAndLogin
         get user_url(-1), headers: @auth_header
         assert_response :not_found
     end
     #End show
     #Create
-    test "Create - created" do
+    test "Create - Created" do
         post users_url, params:{user: attributes_for(:user)}
         assert_response :created
     end
-    test "Create - email duplicated" do
+    test "Create - Email duplicated" do
         @user = create(:user)
         post users_url, params:{user: attributes_for(:user, email: @user.email)}
         assert_response :unprocessable_entity
     end
-    test "Create - username duplicated" do
+    test "Create - User should have an email" do
+        post users_url, params:{user: attributes_for(:user, email:nil)}
+        assert_response :unprocessable_entity
+    end
+    test "Create - Email invalid" do
+        post users_url, params:{user: attributes_for(:user, email:"an_email")}
+        assert_response :unprocessable_entity
+    end
+    test "Create - Username duplicated" do
         @user = create(:user)
         post users_url, params:{user: attributes_for(:user, username: @user.username)}
         assert_response :unprocessable_entity
     end
-    test "Create - username missing" do
+    test "Create - User should have an username" do
         post users_url, params:{user: attributes_for(:user, username:nil)}
         assert_response :unprocessable_entity
     end
-    test "Create - email missing" do
-        post users_url, params:{user: attributes_for(:user, email:nil)}
-        assert_response :unprocessable_entity
-    end
-    test "Create - email invalid" do
-        post users_url, params:{user: attributes_for(:user, email:"an_email")}
-        assert_response :unprocessable_entity
-    end
-    test "Create - password missing" do
+    test "Create - User should have a password" do
         post users_url, params:{user: attributes_for(:user, password:nil)}
         assert_response :unprocessable_entity
     end
-    test "Create - password too short" do
+    test "Create - Password is too short" do
         post users_url, params:{user: attributes_for(:user, password:"short", password_confirmation:"short")}
         assert_response :unprocessable_entity
     end
-    test "Create - password and password_confirmation are diferent" do
+    test "Create - Password and password_confirmation are different" do
         post users_url, params:{user: attributes_for(:user, password:"valid_password", password_confirmation: "Another_valid_password")}
         assert_response :unprocessable_entity
     end
     #End create
     #Update
-    test "Update - :ok" do
+    test "Update - No content" do
         createUserAndLogin
         put user_url(@user), params:{user: {name: 'new name'}}, headers: @auth_header
         assert_response :no_content
     end
-    test "Update - password" do
+    test "Update - Password" do
         createUserAndLogin
         put user_url(@user), params:{user: {password: 'new_password'}}, headers: @auth_header
         assert_response :no_content
     end
-    test "Update - password and password_confirmation are different" do
+    test "Update - Password and password_confirmation are different" do
         createUserAndLogin
         put user_url(@user), params:{user: {password: 'new_password', password_confirmation:"other_password"}}, headers: @auth_header
         assert_response :unprocessable_entity
     end
-    test "Update - user should be logged in" do
+    test "Update - User should be logged in" do
         @user = create(:user)
         put user_url(@user), params:{user: {name: 'new name'}}
         assert_response :unauthorized
     end
-    test "Update - an user only can update himself" do
+    test "Update - User's id and token should match" do
         createUserAndLogin
         createOtherUserAndLogin
         put user_url(@user), params:{user: {name: 'new name'}}, headers: @other_user_auth_header
         assert_response :unauthorized
     end
-    test "Update - user not found" do
+    test "Update - User not found" do
         createUserAndLogin
         put user_url(-1), params:{user: {name: 'new name'}}, headers: @auth_header
         assert_response :not_found
     end
-    test "Update - username duplicated" do
+    test "Update - Username duplicated" do
         createUserAndLogin
-        createOtherUserAndLogin
+        @other_user = create(:user)
         put user_url(@user), params:{user: {username: @other_user.username}}, headers: @auth_header
         assert_response :unprocessable_entity
     end
-    test "Update - email duplicated" do
+    test "Update - Email duplicated" do
         createUserAndLogin
-        createOtherUserAndLogin
+        @other_user = create(:user)
         put user_url(@user), params:{user: {email: @other_user.email}}, headers: @auth_header
+        assert_response :unprocessable_entity
+    end
+    test "Update - User should have an username" do
+        createUserAndLogin
+        put user_url(@user), params:{user: {username: nil}}, headers: @auth_header
+        assert_response :unprocessable_entity
+    end
+    test "Update - User should have an email" do
+        createUserAndLogin
+        put user_url(@user), params:{user: {email: nil}}, headers: @auth_header
         assert_response :unprocessable_entity
     end
     #End update
     #Delete
-    test "Delete - ok" do
+    test "Delete - No content" do
         createUserAndLogin
         delete user_url(@user), headers: @auth_header
         assert_response :no_content
     end
-    test "Delete - should be logged in" do
+    test "Delete - User should be logged in" do
         @user = create(:user)
         delete user_url(@user)
         assert_response :unauthorized
     end
-    test "Delete - an user only can destroy himself" do
+    test "Delete - User's id and token should match" do
         createUserAndLogin
         createOtherUserAndLogin
         delete user_url(@user), headers: @other_user_auth_header
         assert_response :unauthorized
     end
-    test "Delete - should destroy lists" do
+    test "Delete - No content - Should destroy lists if any" do
         createUserAndLogin
         @list = create(:list, user: @user)
         delete user_url(@user), headers: @auth_header
