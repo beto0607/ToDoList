781c5f89ebc277fe1942d12b1cdefa353b3b284f
Roberto Albanesi <beto0607@gmail.comn>
2019-06-20 15:54:59 +0200

    Moved database credentials to a ignored file

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -31,5 +31,12 @@ module ToDoLists
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, "config", "local_env.yml")
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
