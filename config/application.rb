8d50bab64e85aca002a346054cdc46b23eeb345f
Roberto Albanesi <beto0607@gmail.comn>
2019-06-21 22:17:26 +0200

    Added cors

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module ToDoLists
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
+    config.middleware.insert_before 0, Rack::Cors do
+        allow do
+           origins '*'
+           resource '*', :headers => :any, :methods => [:get, :post, :options]
+         end
+      end      
   end
 end
