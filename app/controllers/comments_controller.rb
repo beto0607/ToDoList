bd4e344ccb64d72dcce3e36590b7f8d079a47f96
Roberto Albanesi <beto0607@gmail.comn>
2019-06-23 01:00:49 +0200

    Adder serialized response to Comments cotnroller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
   before_action :authorize_request
   before_action :set_list, only: [:index, :create]
   before_action :set_comment, only: [:show, :update, :destroy]
-  before_action :check_if_owner, only: [:index,:create, :update, :destroy]
+  before_action :check_if_owner, only: [:index, :create, :update, :destroy]
 
   # GET /lists/1/comments
   def index
-    render json: @list.comments
+    render json: serialize_models(@list.comments),
+           status: :ok
   end
 
   # POST /lists/1/comments
@@ -15,18 +16,20 @@ class CommentsController < ApplicationController
     @comment.user = @current_user
     @comment.list = @list
     if @comment.save
-      render json: @comment, status: :created
+      render json: serialize_model(@comment),
+             status: :created
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render_errors(:unprocessable_entity, @comment.errors)
     end
   end
 
   # PATCH/PUT /comments/1
   def update
     if @comment.update(comment_params)
-      render json: @comment, status: :ok
+      render json: serialize_model(@comment),
+             status: :ok
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render_errors(:unprocessable_entity, @comment.errors)
     end
   end
 
@@ -41,7 +44,7 @@ class CommentsController < ApplicationController
   def set_comment
     @comment = Comment.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    render json: ["List not found"], status: :not_found
+    render_error(:not_found, "Comment not found", "Comment with id \"#{params[:id]}\" was not found.")
   end
 
   # Only allow a trusted parameter "white list" through.
@@ -52,14 +55,14 @@ class CommentsController < ApplicationController
   def set_list
     @list = List.find(params[:list_id])
   rescue ActiveRecord::RecordNotFound
-    render json: ["List not found"], status: :not_found
+    render_error(:not_found, "List not found", "List with id \"#{params[:list_id]}\" was not found.")
   end
 
   def check_if_owner
     if ((@comment && List.find(@comment.list_id).owner?(@current_user)) || (@list && @list.owner?(@current_user)))
       true
     else
-      render json: { errors: "User is not the owner of the list" }, status: :unauthorized
+      render_error(:unauthorized, "User is not the owner", "User with id \"#{@current_user.id}\" is not the owner of this list.")
     end
   end
 end
