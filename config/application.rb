bedadf51e35cb6786f98878498e9b96f250a72c0
Roberto Albanesi <beto0607@gmail.comn>
2019-06-22 23:09:02 +0200

    Added put, patch and delete methods in cors

--- config/application.rb
+++ config/application.rb
@@ -39,10 +39,10 @@ module ToDoLists
       end if File.exists?(env_file)
     end
     config.middleware.insert_before 0, Rack::Cors do
-        allow do
-           origins '*'
-           resource '*', :headers => :any, :methods => [:get, :post, :options]
-         end
-      end      
+      allow do
+        origins "*"
+        resource "*", :headers => :any, :methods => [:get, :post, :options, :delete, :put, :patch]
+      end
+    end
   end
 end
