02a51d06e5d4183045fa8b31015ecc078a8d4521
Roberto Albanesi <beto0607@gmail.comn>
2019-06-22 19:53:04 +0200

    Added items_count and items_done_count in Lists responses. Added scope to items to count done item for a list

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
     before_create do#Sets status to "ACTIVE" as default
         self.status = "ACTIVE"
     end
+
+    scope :count_done, ->(list_id){where(list_id: list_id, status: 'DONE').count}
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ApplicationRecord
+    
+    attr_accessor :items_count
+    attr_accessor :items_done_count
 
     belongs_to :user
 
@@ -6,6 +9,19 @@ class List < ApplicationRecord
     has_many :comments, dependent: :destroy
 
     validates :title, presence:true
+
+    def items_count
+        self.items.count
+    end
+
+    def items_done_count
+        Item.count_done(self.id)
+    end
+
+    def as_json(options)
+        super(:methods => [:items_count, :items_done_count])
+    end
+      
 
     def owner? current_user
         self.user == current_user
